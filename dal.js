dd4a4775300f33eeb91bd29dedcc2b02e031b96b
Caspian Alderman <caspian.alderman@gmail.com>
2022-09-11 17:48:40 -0700

    firebase and package update

--- dal.js
+++ dal.js
@@ -19,6 +19,50 @@ function create(name, email, password){
     })
 }
 
+// find user account
+function find(email){
+    return new Promise((resolve, reject) => {    
+        const customers = db
+            .collection('users')
+            .find({email: email})
+            .toArray(function(err, docs) {
+                err ? reject(err) : resolve(docs);
+        });    
+    })
+}
+
+// find user account
+function findOne(email){
+    return new Promise((resolve, reject) => {    
+        const customers = db
+            .collection('users')
+            .findOne({email: email})
+            .then((doc) => resolve(doc))
+            .catch((err) => reject(err));    
+    })
+}
+
+// update - deposit/withdraw amount
+function update(email, amount){
+    return new Promise((resolve, reject) => {    
+        const customers = db
+            .collection('users')            
+            .findOneAndUpdate(
+                {email: email},
+                { $inc: { balance: amount}},
+                { returnOriginal: false },
+                function (err, documents) {
+                    err ? reject(err) : resolve(documents);
+                }
+            );            
+
+
+    });    
+}
+
+
+
+
 function all(){
     return new Promise((resolve, reject) => {    
         const customers = db
@@ -30,4 +74,4 @@ function all(){
     })
 }
 
-module.exports = {create, all};
+module.exports = {create, findOne, find, update, all};
